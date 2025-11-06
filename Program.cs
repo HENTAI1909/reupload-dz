bc0baa2f57d8c5f9499cf63c384209807123d117
HENTAI1909 <113054255+HENTAI1909@users.noreply.github.com>
2025-11-06 20:44:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Средний уровень вариант 2 (страница 45)
+//пост и пред условие
+try
+{
+    Console.WriteLine("Четные числа кратные 5");
+
+    // предусловие
+    int i = 10;
+    while (i <= 100)
+    {
+        Console.Write(i + " ");
+        i += 10;
+    }
+
+    Console.WriteLine();
+
+    //постусловие
+    int j = 10;
+    do
+    {
+        Console.Write(j + " ");
+        j += 10;
+    }
+    while (j <= 100);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
