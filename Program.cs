7bace94f055a61a1a2a26e996dd17a0939161044
HENTAI1909 <113054255+HENTAI1909@users.noreply.github.com>
2025-11-06 20:44:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//базовый вариант 2 стр 51
+try
+{
+    double x = 1.0;
+    Console.WriteLine("x     |    y");
+    while (x <= 3.0)
+    {
+        double y = x * x - Math.Cos(Math.PI * x) * Math.Cos(Math.PI * x);
+        Console.WriteLine($"{x:F1}\t{y:F4}");
+        x += 0.2;
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
